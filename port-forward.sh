baf9f282c4e6e4c11352d2b0f2b4e45fc3da8868
VisaZhou <472493922@qq.com>
2025-04-08 23:19:12 +0800

    【Port】端口绑定.sh

---
+++ port-forward.sh
@@ -0,0 +1,4 @@
+nohup kubectl port-forward pod/nacos-cluster-0 8848:8848 > port-forward.log 2>&1 &
+nohup kubectl port-forward pod/mysql-cluster-0 3306:3306 > port-forward.log 2>&1 &
+nohup kubectl port-forward pod/mysql-cluster-0 6379:6379 > port-forward.log 2>&1 &
+
