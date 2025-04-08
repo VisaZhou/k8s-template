50959ab56a699eec9039e8e5e99d75d7a7adbef8
VisaZhou <472493922@qq.com>
2025-04-08 23:51:48 +0800

    【Port】nacos多端口映射

--- port-forward.sh
+++ port-forward.sh
@@ -1,4 +1,4 @@
-nohup kubectl port-forward pod/nacos-cluster-0 8848:8848 > port-forward.log 2>&1 &
+nohup kubectl port-forward pod/nacos-cluster-0 8848:8848 9848:9848 9849:9849 > port-forward.log 2>&1 &
 nohup kubectl port-forward pod/mysql-cluster-0 3306:3306 > port-forward.log 2>&1 &
 nohup kubectl port-forward pod/redis-cluster-0 6379:6379 > port-forward.log 2>&1 &
 
